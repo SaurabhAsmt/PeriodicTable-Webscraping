c69bcb37ac2ed1e958779d753b3958a1a3dc76bc
usmit <moskau789@gmail.com>
2025-01-15 12:07:04 +0530

    added comment

--- periodic_tb/periodic_tb/pipelines.py
+++ periodic_tb/periodic_tb/pipelines.py
@@ -2,7 +2,7 @@ import sqlite3
 from itemadapter import ItemAdapter
 import copy
 import json
-import logging #for debugging
+import logging #for debugging purpose
 
 class GroupedElmsPipeline:
     def __init__(self):
